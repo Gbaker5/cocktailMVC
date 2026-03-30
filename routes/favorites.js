730ca1afb3ded2a733375c8a27b10b94099e1a1e
Gjarred Baker <gjarred23@gmail.com>
2026-03-30 15:36:00 -0500

    updated favorites routes

--- routes/favorites.js
+++ routes/favorites.js
@@ -2,11 +2,12 @@ const express = require('express')
 const router = express.Router()
 const homeController = require('../controllers/home')
 const authController = require("../controllers/auth");
+const favoritesController = require("../controllers/favorites");
 const { ensureAuth, ensureGuest } = require("../middleware/auth");
 
-router.get('/favorites', ensureAuth,homeController.getFavorites)
-router.put('/favorites/:id', ensureAuth,homeController.putFavorites)
-router.delete('/favorites/:id', ensureAuth,homeController.deleteFavorites)
+router.get('/favorites', ensureAuth, favoritesController.getFavorites)
+router.put('/favorites/:id', ensureAuth, favoritesController.putFavorites)
+router.delete('/favorites/:id', ensureAuth, favoritesController.deleteFavorites)
 
 
 
